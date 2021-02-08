1f40794be9fb212fbf9163b0a4d530e422bb86b0
MONASH\rgreg <greg.rolan@monash.edu>
2021-02-09 09:20:13 +1100

    themes, plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,10 +20,9 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Site by Pelican', 'https://getpelican.com/'),
+         ('Pelican uses Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         )
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -33,10 +32,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
-DISPLAY_PAGES_ON_MENU = False
-
-DISPLAY_CATEGORIES_ON_MENU = False
 
 MARKDOWN = {
     'extension_configs': {
@@ -48,9 +43,48 @@ MARKDOWN = {
     'lazy_ol': False 
 }
 
+DISPLAY_PAGES_ON_MENU = False
+
+DISPLAY_CATEGORIES_ON_MENU = False
+
 MENUITEMS = (
-  ('Home','/'),
   ('About The Lab','/pages/about.html'),
+  ('Publications','/pages/publications.html'),
   ('Contact','/pages/contact.html')
 )
 
+#THEME = 'notmyidea'
+THEME = './themes/voidy'
+
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ["tag_cloud"]
+
+
+######################################################################
+# VOIDY
+SITETAG = "AiLECS Lab"
+
+# Extra stylesheets, for bootstrap overrides or additional styling.
+STYLESHEET_FILES = ("pygment.css", "voidybootstrap.css","tagcloud.css")
+
+# Put taglist at end of articles, and use the default sharing button implementation.
+CUSTOM_ARTICLE_FOOTERS = ("taglist.html", "sharing.html", )
+CUSTOM_SCRIPTS_ARTICLE = "sharing_scripts.html"
+
+# Default sidebar template. Omit this setting for single column mode without sidebar.
+SIDEBAR = "sidebar.html"
+
+CUSTOM_SIDEBAR_MIDDLES = ("sb_tagcloud.html", )
+TAG_CLOUD_STEPS = 4	
+TAG_CLOUD_MAX_ITEMS = 100
+TAG_CLOUD_SORTING = 'random'
+TAG_CLOUD_BADGE = False
+
+SOCIAL = (('Twitter', 'https://twitter.com/username',
+         'fa fa-twitter-square fa-fw fa-lg'),
+        ('LinkedIn', 'http://linkedin-url',
+         'fa fa-linkedin-square fa-fw fa-lg'),
+        ('GitHub', 'http://github.com/username',
+         'fa fa-github-square fa-fw fa-lg'),
+        )
+######################################################################
